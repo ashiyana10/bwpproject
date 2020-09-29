88a5c52c2a31759b68b12d227e3ed7c634109bc3
Ashiyana <ashiyanahalvadiya123@gmail.com>
2020-09-29 06:21:38 +0530

    project

---
+++ apply.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="index.css">
+	<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>  
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+ 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"> 
+ 	<style type="text/css">
+ 		.opacity1{
+ 			opacity: 0.7;
+ 		}
+ 	</style>
+
+</head>
+<body background="images\fort.jpg" style="background-repeat: no-repeat;background-size: cover;background-attachment: fixed;">
+<div class="container"><br><br>
+	<div class="jumbotron opacity1">
+		<center> <h3>Apply For Voter Or Candidate</center><br><br>
+		<form class="form-inline" role="form" >
+			<div class="row">
+				<div class="col-sm-4">
+					<input type="text" name="surnm" class="form-control" placeholder="Surname" style="width: 100%">
+				</div>
+				<div class="col-sm-4">
+					<input type="text" name="mnm" class="form-control" placeholder="Middle Name" style="width: 100%">
+				</div>
+				<div class="col-sm-4">
+					<input type="text" name="lnm" class="form-control" placeholder="Last Name" style="width: 100%">
+				</div>
+			</div><br>
+			<div class="row">
+				<div class="col-sm-12">
+					<input type="text" name="fnm" class="form-control" placeholder="Father Full Name" style="width: 100%">
+				</div>
+			</div><br>
+			<div class="row">
+				<div class="col-sm-12">
+					<input type="tel" name="email" class="form-control" placeholder="Contact No" style="width: 100%">
+				</div>
+			</div><br>
+			<div class="row">
+				<div class="col-sm-12">
+					<input type="email" name="email" class="form-control" placeholder="E-mail" style="width: 100%">
+				</div>
+			</div><br>
+			Birth Date:<br>
+			<div class="row">
+				<div class="col-sm-12">
+					<input type="date" name="date" class="form-control" placeholder="E-mail" style="width: 100%">
+				</div>
+			</div><br>
+			<div class="row">
+				<div class="col-sm-12">
+					<textarea  rows="3" style="width: 100%" class="form-control" placeholder="Current Address">
+						
+					</textarea>
+				</div>
+			</div><br>
+			Gender:<br>
+			<div class="row">
+				<div class="col-sm-2">
+					<input type="radio" name="Gender" value="male" class="form-control">&nbsp;&nbsp;Male
+				</div>
+				<div class="col-sm-2">
+					<input type="radio" name="Gender" value="female" class="form-control">&nbsp;&nbsp;Female
+				</div>
+			</div><br>
+			Current City:<br>
+			<div class="row">
+				<div class="col-sm-12">
+					<select class="form-control" style="width: 100%">
+						<option>City</option>
+						<option>Jamnagar</option>
+						<option>Rajkot</option>
+						<option>Ahemdabad</option>
+						<option>Vadodra</option>
+						<option>Surat</option>
+					</select>
+				</div>
+			</div><br>
+			<div class="row">
+				<div class="col-sm-12">
+					<select class="form-control" style="width: 100%">
+						<option>Urban</option>
+						<option>Rubal</option>
+					</select>
+				</div>
+			</div><br>
+			<div class="row">
+				<div class="col-sm-4">
+					<button type="button" class="btn btn-primary form-control" style="width: 100%">Submit</button>
+				</div>
+				<div class="col-sm-4">
+					<button type="button" class="btn btn-default form-control" style="width: 100%">Clear</button>
+				</div>
+				<div class="col-sm-4">
+					<button type="button" class="btn btn-danger form-control" style="width: 100%">Cancel</button>
+				</div>
+			</div>
+		</form>
+	</div>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>E-voting</title>
+	<link rel="stylesheet" type="text/css" href="index.css">
+	<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>  
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"> 
+ <style type="text/css">
+ 	body{
+ 		color: black;
+ 	}
+ </style>
+</head>
+
+<body>
+
+<div class="main" style="background-color:#E5E4E2">
+
+	<div class="first-section">
+			
+	
+			<div id="myCarousel" class="carousel slide" data-ride="carousel">
+ 			<!-- Indicators -->
+	 			<ol class="carousel-indicators">
+ 					<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+ 					<li data-target="#myCarousel" data-slide-to="1"></li>
+ 					<li data-target="#myCarousel" data-slide-to="2"></li>
+ 				</ol>
+ 				<!-- Wrapper for slides -->
+ 				<div class="carousel-inner" >
+ 					<div class="item active" style="background-image: url('images/pm_ashu.png');">
+ 	 					<?php include 'header.html'; ?>
+ 	 					<p style="padding: 200px;font-size: 30px" >Election 2020-2021</p>
+ 					</div>
+ 					<div class="item" style="background-image: url('images/cm_new.jpg');">
+		 				<?php include 'header.html'; ?> 
+		  				<p style="padding: 200px;font-size: 30px" >Election 2020-2021</p>
+ 					</div>
+ 					<div class="item" style="background-image: url('images/pm2.jpg');background-repeat: no-repeat;">
+ 		 				<?php include 'header.html'; ?>
+ 		  				<p style="padding: 200px;font-size: 30px" >Election 2020-2021</p>
+ 					</div>
+ 				</div>
+ 				<!-- Left and right controls -->
+ 					<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+ 						<span class="glyphicon glyphicon-chevron-left"></span>
+ 						<span class="sr-only">Previous</span>
+ 					</a>
+ 					<a class="right carousel-control" href="#myCarousel" data-slide="next">
+ 						<span class="glyphicon glyphicon-chevron-right"></span>
+ 						<span class="sr-only">Next</span>
+ 					</a>
+			</div>
+	</div>
+
+	<div class="container-fluid" style="background-color:white;margin: 20px;margin-top: 0px">
+		<div class="row">
+			<div class="col-sm-12">
+				 <p style="margin-top: 15px;"><b><h3><center>Political Parties</center></h3></b></p>
+			</div>
+		</div><br>
+		<div class="row">
+			<div class="col-sm-4">
+				<a href=""><img src="images/bjp.png" width="400px" height="300px"></a><br/><br/>
+				<p><center><b>Bharatiya Janata Party(BJP)</b></center></p>
+			</div>
+			<div class="col-sm-4">
+				<a href=""><img src="images/cong1.jpg" width="400px" height="300px"></a><br><br>
+				<p><center><b>Indian National Congress(INC)</b></center></p>
+			</div>
+			<div class="col-sm-4">
+				<a href=""><img src="images/ncp.jpg" width="400px" height="300px"></a><br><br>
+				<p><center><b>Nationalist Congress Party(NCP)</b></center></p>
+			</div>
+		</div>
+	</div>
+
+	
+</div>
+
+<div id="loginModal" class="modal fade" role="dialog" >
+
+ 	<div class="modal-dialog">
+ 		<!-- Modal content-->
+ 		<div class="modal-content">
+ 			<div class="modal-header">
+ 				<button type="button" class="close" data-dismiss="modal">&times;</button>
+ 				<h4 class="modal-title"><center>Login</center></h4>
+ 			</div>
+ 			<div class="modal-body">
+ 				<form class="form-inlne" role="form">
+ 					<input type="text" class="form-control" name="id" placeholder="Enter id"><br>
+ 					<input type="password" class="form-control" name="pwd" placeholder="Password">
+ 				</form>
+ 			</div>
+ 			<div class="modal-footer">
+ 				<button type="button" class="btn btn-primary" >Login</button>
+ 				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+ 			</div>
+ 		</div>
+ 	</div>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>  
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script> 
+</body>
+</html>
