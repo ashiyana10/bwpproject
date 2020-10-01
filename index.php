d3f444601cbfb2279c0bcc2fc58f46f661f5a611
Ashiyana <ashiyanahalvadiya123@gmail.com>
2020-10-01 09:02:06 +0530

    1octo

--- index.php
+++ index.php
@@ -11,6 +11,11 @@
  	body{
  		color: black;
  	}
+ .re1	 .capsan:after {border:solid 2px #000; top:30px; left:40px; bottom:30px; right:30px; content:""; position:absolute;}
+  .re1 .capsan {position:absolute; top:0px; left:0px; width:100%; text-align:center; height:100%; -moz-transform:scale(0);-ms-transform:scale(0);-o-transform:scale(0);-webkit-transform:scale(0); transform:scale(0); -moz-transition:opacity 0.35s ease 0s, transform 0.35s ease 0s; -ms-transition:opacity 0.35s ease 0s, transform 0.35s ease 0s;-o-transition:opacity 0.35s ease 0s, transform 0.35s ease 0s;-webkit-transition:opacity 0.35s ease 0s, transform 0.35s ease 0s;transition:opacity 0.35s ease 0s, transform 0.35s ease 0s; opacity:0;}
+ .re1:hover .capsan {-moz-transform:scale(1);-ms-transform:scale(1);-o-transform:scale(1);-webkit-transform:scale(1); transform:scale(1); opacity:1;}
+ .re1:hover img {opacity:0.7;}
+  .re1 .capsan span {position:absolute; top:50%; left:0px; width:100%; text-align:center; line-height:30px; font-size:20px; color:#000000; margin-top:-15px;}
  </style>
 </head>
 
@@ -30,11 +35,11 @@
  				</ol>
  				<!-- Wrapper for slides -->
  				<div class="carousel-inner" >
- 					<div class="item active" style="background-image: url('images/pm_ashu.png');">
+ 					<div class="item active" style="background-image: url('images/pm_pics.png');">
  	 					<?php include 'header.html'; ?>
  	 					<p style="padding: 200px;font-size: 30px" >Election 2020-2021</p>
  					</div>
- 					<div class="item" style="background-image: url('images/cm_new.jpg');">
+ 					<div class="item" style="background-image: url('images/cm_pics.png');">
 		 				<?php include 'header.html'; ?> 
 		  				<p style="padding: 200px;font-size: 30px" >Election 2020-2021</p>
  					</div>
@@ -59,6 +64,7 @@
 		<div class="row">
 			<div class="col-sm-12">
 				 <p style="margin-top: 15px;"><b><h3><center>Political Parties</center></h3></b></p>
+				 <p style="margin-top: 15px;"><b><h3><center>Political Parties</center></h3></b></p>
 			</div>
 		</div><br>
 		<div class="row" >
@@ -80,28 +86,36 @@
 
 
 
-	<div class="container-fluid" id="Results" style="background-color:white;margin: 20px;margin-top: 0px">
-		<div class="row">
-			<div class="col-sm-12">
+	<div class="container-fluid result" id="Results" style="background-color:white;margin: 20px;margin-top: 0px">
+		<div class="row" style="height:300px;width:1310px;background-image: url('images/flag2_new.jpg');background-size: cover;">
+			<h2><p style="padding: 90px">Results for the older year will be checkout here area wisw</p></h2>
+
+			<!--<div class="col-sm-12">
 				 <p style="margin-top: 15px;"><b><h3><center>Results</center></h3></b></p>
 			</div>
 		</div><br>
-		<div class="row" >
+		<div class="row ">
 			<div class="col-sm-4">
-				<div style="height:300px;width: 410px;background-image: url('images/flag.jpg');background-size: cover;">
-					<p></p>
+				<div class= "re1" style="height:300px;width: 410px;background-image: url('images/flag.jpg');background-size: cover;">
+					<span class="capsan">
+                		<span><b>2020-2021</b></span>
+         			</span>
 				</div>
 			</div>
 			<div class="col-sm-4">
-				<div style="height:300px;width: 410px;background-image: url('images/flag.jpg');background-size: cover;">
-					
+				<div class= "re1" style="height:300px;width: 410px;background-image: url('images/flag.jpg');background-size: cover;">
+					<span class="capsan">
+                		<span><b>2019-2020</b></span>
+         			</span>
 				</div>
 			</div>
 			<div class="col-sm-4">
-				<div style="height:300px;width: 410px;background-image: url('images/flag.jpg');background-size: cover;">
-					
+				<div class= "re1" style="height:300px;width: 410px;background-image: url('images/flag.jpg');background-size: cover;">
+					<span class="capsan">
+                		<span><b>2018-2019</b></span>
+         			</span>
 				</div>
-			</div>
+			</div>-->
 		</div>
 	</div>
 
